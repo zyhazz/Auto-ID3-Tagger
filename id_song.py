72ae9bae14e00444c7c2e47766e899b5e1b48337
Diego Carvalho <zyhazz@gmail.com>
2026-02-15 21:50:19 -0300

    a bunch of improvements

--- id_song.py
+++ id_song.py
@@ -7,78 +7,88 @@ import eyed3.plugins.art
 
 from shazamio import Shazam, Serialize
 
-MUSIC_FOLDER = ""
+MUSIC_FOLDER = "/Users/diegocarvalho/Music/Music/Media.localized/Music/Unknown"
 
 
 async def main():
     shazam = Shazam()
-    files = os.listdir(MUSIC_FOLDER)
-    for file in files:
-        print(file)
-        # Loop over files and filter on MP3 files
-        path = os.path.join(MUSIC_FOLDER, file)
-        if not os.path.isfile(path):
-            continue
-        if path[-3:] != "mp3":
-            continue
+    for root, _dirs, files in os.walk(MUSIC_FOLDER):
+        for file in files:
+            path = os.path.join(root, file)
+            if not path.lower().endswith(".mp3"):
+                continue
+            print(path)
 
-        # Identify song using Shazam
-        out = await shazam.recognize_song(path)
-        if len(out['matches']) < 1:
-            print("Shazam could not find a match")
-            continue
-        data = Serialize.track(out['track'])
+            # Identify song using Shazam
+            out = await shazam.recognize(path)
+            if len(out['matches']) < 1:
+                print("Shazam could not find a match")
+                continue
+            data = Serialize.track(out['track'])
 
-        # Extract data from Shazam response
-        tags = {}
-        tags['title'] = out['track']['title']
-        tags['artist'] = out['track']['subtitle']
-        tags['genre'] = out['track']['genres']['primary']
+            # Extract data from Shazam response (use .get() for optional fields)
+            track = out['track']
+            tags = {}
+            tags['title'] = track.get('title', '')
+            tags['artist'] = track.get('subtitle', '')
+            tags['genre'] = (track.get('genres') or {}).get('primary', '')
+            tags['album'] = ""
+            tags['year'] = ""
 
-        cover_art = out['track']['images']['coverarthq'].replace("400x400", "1000x1000")
+            cover_art = (track.get('images') or {}).get('coverarthq')
+            if cover_art:
+                cover_art = cover_art.replace("400x400", "1000x1000")
 
-        for section in data.sections:
-            if section.type == "SONG":
-                for md in section.metadata:
-                    if md.title == "Album":
-                        tags['album'] = md.text
-                    if md.title == "Released":
-                        tags['year'] = md.text
+            for section in data.sections:
+                if section.type == "SONG":
+                    for md in section.metadata:
+                        if md.title == "Album":
+                            tags['album'] = md.text
+                        if md.title == "Released":
+                            tags['year'] = md.text
 
-        print("Shazam: " + tags['artist'] + " - " + tags['title'] + " (" + tags['album'] + ")")
+            print("Shazam: " + tags['artist'] + " - " + tags['title'] + " (" + tags['album'] + ")" if tags['album'] else "Shazam: " + tags['artist'] + " - " + tags['title'])
 
-        # Reset current tags
-        id3 = eyed3.load(path)
-        if id3.tag:
-            id3.tag.clear()
-            id3.tag.save()
-        else:
-            id3.initTag()
-            id3.tag.save()
+            # Reset current tags
+            id3 = eyed3.load(path)
+            if id3.tag:
+                id3.tag.clear()
+                id3.tag.save()
+            else:
+                id3.initTag()
+                id3.tag.save()
 
-        # Save new tags
-        id3 = eyed3.load(path)
-        id3.tag.album = tags['album']
-        id3.tag.artist = tags['artist']
-        id3.tag.album_artist = tags['artist']
-        id3.tag.genre = tags['genre']
-        id3.tag.title = tags['title']
-        id3.tag.year = tags['year']
+            # Save new tags
+            id3 = eyed3.load(path)
+            id3.tag.album = tags['album']
+            id3.tag.artist = tags['artist']
+            id3.tag.album_artist = tags['artist']
+            id3.tag.genre = tags['genre']
+            id3.tag.title = tags['title']
+            id3.tag.year = tags['year']
 
-        # Download art and (temporary) save locally
-        local_art = os.path.join(MUSIC_FOLDER, "tmp_file" + cover_art[-4:])
-        urllib.request.urlretrieve(cover_art, local_art)
+            # Download art and (temporary) save locally (same dir as the MP3)
+            if cover_art:
+                local_art = os.path.join(root, "tmp_file" + cover_art[-4:])
+                try:
+                    urllib.request.urlretrieve(cover_art, local_art)
 
-        # Save art to MP3 file
-        id3_front_cover_id = eyed3.utils.art.TO_ID3_ART_TYPES['FRONT_COVER'][0]
-        id3_cover = eyed3.plugins.art.ArtFile(local_art)
-        id3_cover.id3_art_type = id3_front_cover_id
-        id3.tag.images.set(id3_cover.id3_art_type, id3_cover.image_data, id3_cover.mime_type)
+                    # Save art to MP3 file
+                    id3_front_cover_id = eyed3.utils.art.TO_ID3_ART_TYPES['FRONT_COVER'][0]
+                    id3_cover = eyed3.plugins.art.ArtFile(local_art)
+                    id3_cover.id3_art_type = id3_front_cover_id
+                    id3.tag.images.set(id3_cover.id3_art_type, id3_cover.image_data, id3_cover.mime_type)
 
-        # Save the new tags
-        id3.tag.save(version=(2, 3, 0))
+                    # Save the new tags
+                    id3.tag.save(version=(2, 3, 0))
 
-        print("Saved new tags")
+                    print("Saved new tags")
+                finally:
+                    if os.path.isfile(local_art):
+                        os.remove(local_art)
+            else:
+                id3.tag.save(version=(2, 3, 0))
+                print("Saved new tags (no cover art)")
 
-loop = asyncio.get_event_loop()
-loop.run_until_complete(main())
+if __name__ == "__main__":
+    asyncio.run(main())
